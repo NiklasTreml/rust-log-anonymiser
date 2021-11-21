eb9cee3dca81e214cdc427aad10a9b7da4de9053
Niklas Treml <treml.niklas@gmail.com>
2021-11-21 02:02:50 +0100

    added single threaded recursion

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use core::time;
 use std::{
     env, fs,
-    fs::File,
+    fs::{DirEntry, File},
     io::{Read, Write},
     ops::Add,
     path::Path,
-    sync::mpsc,
+    sync::mpsc::{self, Sender},
     thread,
     time::{Duration, SystemTime},
 };
@@ -13,32 +13,68 @@ use std::{
 use indicatif::{ProgressBar, ProgressStyle};
 use rand::Rng;
 use regex::Regex;
-const OUTPUTPATH: &str = "./output";
 fn main() {
     let script_start = SystemTime::now();
     let mut args: Vec<String> = env::args().collect();
     args.reverse();
     args.pop();
     args.reverse();
+    let OUTPUTPATH = args[1].as_str();
     let len = args.len();
-    let mut handles = vec![];
+    // let mut handles = vec![];
     let (tx, rx) = mpsc::channel();
     // delete output folder
-    if Path::new(OUTPUTPATH).exists() {
-        fs::remove_dir_all(OUTPUTPATH).expect("Failed to remove output folder");
-        println!("Deleted {}", OUTPUTPATH);
-    }
-
     let (barTx, barRx) = mpsc::channel();
-    let pb = ProgressBar::new(len as u64);
+    let pb = ProgressBar::new_spinner();
     pb.set_style(
-        ProgressStyle::default_bar()
-            .template(
-                "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}]  [{pos}/{len}] {msg}",
-            )
-            .progress_chars("=>-"),
+        ProgressStyle::default_spinner()
+            // For more spinners check out the cli-spinners project:
+            // https://github.com/sindresorhus/cli-spinners/blob/master/spinners.json
+            .tick_strings(&[
+                "⠀", "⠁", "⠂", "⠃", "⠄", "⠅", "⠆", "⠇", "⡀", "⡁", "⡂", "⡃", "⡄", "⡅", "⡆", "⡇",
+                "⠈", "⠉", "⠊", "⠋", "⠌", "⠍", "⠎", "⠏", "⡈", "⡉", "⡊", "⡋", "⡌", "⡍", "⡎", "⡏",
+                "⠐", "⠑", "⠒", "⠓", "⠔", "⠕", "⠖", "⠗", "⡐", "⡑", "⡒", "⡓", "⡔", "⡕", "⡖", "⡗",
+                "⠘", "⠙", "⠚", "⠛", "⠜", "⠝", "⠞", "⠟", "⡘", "⡙", "⡚", "⡛", "⡜", "⡝", "⡞", "⡟",
+                "⠠", "⠡", "⠢", "⠣", "⠤", "⠥", "⠦", "⠧", "⡠", "⡡", "⡢", "⡣", "⡤", "⡥", "⡦", "⡧",
+                "⠨", "⠩", "⠪", "⠫", "⠬", "⠭", "⠮", "⠯", "⡨", "⡩", "⡪", "⡫", "⡬", "⡭", "⡮", "⡯",
+                "⠰", "⠱", "⠲", "⠳", "⠴", "⠵", "⠶", "⠷", "⡰", "⡱", "⡲", "⡳", "⡴", "⡵", "⡶", "⡷",
+                "⠸", "⠹", "⠺", "⠻", "⠼", "⠽", "⠾", "⠿", "⡸", "⡹", "⡺", "⡻", "⡼", "⡽", "⡾", "⡿",
+                "⢀", "⢁", "⢂", "⢃", "⢄", "⢅", "⢆", "⢇", "⣀", "⣁", "⣂", "⣃", "⣄", "⣅", "⣆", "⣇",
+                "⢈", "⢉", "⢊", "⢋", "⢌", "⢍", "⢎", "⢏", "⣈", "⣉", "⣊", "⣋", "⣌", "⣍", "⣎", "⣏",
+                "⢐", "⢑", "⢒", "⢓", "⢔", "⢕", "⢖", "⢗", "⣐", "⣑", "⣒", "⣓", "⣔", "⣕", "⣖", "⣗",
+                "⢘", "⢙", "⢚", "⢛", "⢜", "⢝", "⢞", "⢟", "⣘", "⣙", "⣚", "⣛", "⣜", "⣝", "⣞", "⣟",
+                "⢠", "⢡", "⢢", "⢣", "⢤", "⢥", "⢦", "⢧", "⣠", "⣡", "⣢", "⣣", "⣤", "⣥", "⣦", "⣧",
+                "⢨", "⢩", "⢪", "⢫", "⢬", "⢭", "⢮", "⢯", "⣨", "⣩", "⣪", "⣫", "⣬", "⣭", "⣮", "⣯",
+                "⢰", "⢱", "⢲", "⢳", "⢴", "⢵", "⢶", "⢷", "⣰", "⣱", "⣲", "⣳", "⣴", "⣵", "⣶", "⣷",
+                "⢸", "⢹", "⢺", "⢻", "⢼", "⢽", "⢾", "⢿", "⣸", "⣹", "⣺", "⣻", "⣼", "⣽", "⣾", "⣿",
+            ])
+            .template("{spinner:.green} {msg}"),
     );
-    for arg in args {
+
+    let patterns = vec![r"[Ee]nim", r"[Aa]met"];
+    let replacers = vec!["ENIM_REPLACE", "AMET_REPLACE"];
+
+    if Path::new(OUTPUTPATH).exists() {
+        pb.set_message(format!("{} already exists", args[1]));
+        thread::sleep(Duration::from_secs(1));
+        pb.set_message(format!("Deleting {} ...", args[1]));
+        fs::remove_dir_all(OUTPUTPATH).expect("Failed to remove output folder");
+        pb.set_message(format!("Successfully deleted {}!", args[1]));
+        thread::sleep(Duration::from_secs(1));
+    }
+    pb.set_message("Getting to work...");
+    pb.set_position(0);
+    pb.enable_steady_tick(150);
+    visit(
+        args[0].clone(),
+        tx.clone(),
+        barTx.clone(),
+        &patterns,
+        &replacers,
+        OUTPUTPATH,
+    );
+
+    /* for arg in args {
         let sender = tx.clone();
         let barSender = barTx.clone();
 
@@ -61,20 +97,26 @@ fn main() {
         });
 
         handles.push(handle)
-    }
+    } */
     drop(barTx);
 
     for received in barRx {
         if received == 1 {
-            pb.inc(received);
+            // pb.inc(received);
         }
     }
 
     let mut durations: Vec<u128> = vec![];
-    for handle in handles {
+    /* for handle in handles {
         handle.join().unwrap();
-    }
+    } */
 
+    pb.set_style(
+        ProgressStyle::default_spinner()
+            // For more spinners check out the cli-spinners project:
+            // https://github.com/sindresorhus/cli-spinners/blob/master/spinners.json
+            .template("✓ {msg}"),
+    );
     pb.finish_with_message("Done");
     drop(tx);
 
@@ -97,6 +139,47 @@ fn main() {
     );
 }
 
+fn visit(
+    path: String,
+    durationTx: Sender<u128>,
+    barTx: Sender<u64>,
+    patterns: &Vec<&str>,
+    replacers: &Vec<&str>,
+    outputPath: &str,
+) {
+    let p = Path::new(path.as_str());
+
+    if p.is_dir() {
+        let paths = p.read_dir().unwrap();
+        for path in paths {
+            let path = path.unwrap();
+            let path = path.path();
+            let path = path.to_str().unwrap();
+
+            visit(
+                String::from(path),
+                durationTx.clone(),
+                barTx.clone(),
+                patterns,
+                replacers,
+                outputPath,
+            );
+        }
+    } else {
+        let start = SystemTime::now();
+        if anon_file(&p, patterns, replacers, outputPath) {
+            let end = SystemTime::now();
+            let duration = end.duration_since(start).unwrap();
+            // println!("Finished for {}! Took {}ms", &arg, duration.as_millis());
+
+            durationTx
+                .send(duration.as_micros())
+                .expect("Failed to send into channel");
+            barTx.send(1);
+        }
+    }
+}
+
 fn get_avg(durations: Vec<u128>) -> f32 {
     let mut total = 0;
     for duration in &durations {
@@ -106,10 +189,10 @@ fn get_avg(durations: Vec<u128>) -> f32 {
     (total / (durations.len() as u128)) as f32 / 1000.0
 }
 
-fn anon_file(path: &String, patterns: Vec<&str>, replacers: Vec<&str>) -> bool {
+fn anon_file(path: &Path, patterns: &Vec<&str>, replacers: &Vec<&str>, outputPath: &str) -> bool {
     // read original file to variable
 
-    if Path::new(path).is_dir() {
+    if path.is_dir() {
         // println!("Skipping folder {:?}...", path);
         return false;
     }
@@ -123,7 +206,7 @@ fn anon_file(path: &String, patterns: Vec<&str>, replacers: Vec<&str>) -> bool {
         );
     }
     // save_to_file(&variable)
-    save_to_file(content, path);
+    save_to_file(content, path, outputPath);
     return true;
 }
 
@@ -134,7 +217,7 @@ fn modify(content: &mut String, rep: &str, pattern: Regex) {
     //println!("Replaced {} in {:?}", &pattern, new_filepath)
 }
 
-fn save_to_file(content: String, path: &String) {
+fn save_to_file(content: String, path: &Path, outputPath: &str) {
     let old_path = Path::new(path);
 
     let new_filename = old_path
@@ -147,7 +230,7 @@ fn save_to_file(content: String, path: &String) {
         .expect("Could not get parents")
         .to_str()
         .expect("Could not convert folders to str");
-    let string_path = format!("{}/{}/{}", OUTPUTPATH, folders, new_filename);
+    let string_path = format!("{}/{}/{}", outputPath, folders, new_filename);
 
     let new_filepath = Path::new(&string_path);
 
@@ -156,9 +239,7 @@ fn save_to_file(content: String, path: &String) {
     let mut new_f = File::create(new_filepath).unwrap();
     new_f.write_all(content.as_bytes()).unwrap();
 }
-fn read_from_file(filepath: &String) -> String {
-    let filepath = Path::new(&filepath);
-
+fn read_from_file(filepath: &Path) -> String {
     //println!("Running for {:?}", filepath);
 
     let mut f = File::open(filepath).expect("Could not open file.");
